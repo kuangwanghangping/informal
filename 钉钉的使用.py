c44e5525d7cfbac19d4d2ae9957894d102675861
kuangwanghangping <142001244+kuangwanghangping@users.noreply.github.com>
2024-04-06 15:14:10 +0800

    Add files via upload

---
+++ 钉钉的使用.py
@@ -0,0 +1,3 @@
+from dingtalkchatbot.chatbot import DingtalkChatbot
+bot = DingtalkChatbot('https://oapi.dingtalk.com/robot/send?access_token=082ba886fbdb26a948334281cb0bf29de134b56622aa8b2d28a357ff2e4c5263',"SEC2fc5bf41bf77b8e962b467101b41bf0d16ccbffa16c05e7ebcc0b99a6dc0e3dc")
+bot.send_text('sss')
